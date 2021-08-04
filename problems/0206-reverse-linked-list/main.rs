e464442268bf130c7294f09f0bb88cb35c0dfb6c
神楽坂帕琪 <mudream4869@gmail.com>
2021-08-04 15:00:22 +0800

    Create main.rs

---
+++ problems/0206-reverse-linked-list/main.rs
@@ -0,0 +1,28 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut tail = None;
+        let mut prc = head;
+        while let Some(mut node) = prc {
+            prc = node.next;
+            node.next = tail;
+            tail = Some(node);
+        }
+        tail
+    }
+}
